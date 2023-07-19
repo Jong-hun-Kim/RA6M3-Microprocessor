8428cca6d75a1f7e04992ea11f1cebd890e3e521
Jonghun Kim <jhkim2022@g.skku.edu>
2023-07-19 18:43:37 +0900

    Create vocabulary.h

---
+++ Assignment 3/vocabulary.h
@@ -0,0 +1,64 @@
+/*
+ * vocabulary.h
+ *
+ *  Created on: 2023. 2. 9.
+ *      Author: J.H.Kim
+ */
+
+#ifndef VOCABULARY_H_
+#define VOCABULARY_H_
+
+const char *voca[50] = {
+                           "dog", // 0
+                           "elephant",
+                           "horse",
+                           "apple",
+                           "schedule",
+                           "route",
+                           "robber",
+                           "bowl",
+                           "symbol",
+                           "handshake",
+                           "master", // 10
+                           "officer",
+                           "semester",
+                           "telephone",
+                           "pair",
+                           "living room",
+                           "problem",
+                           "garage",
+                           "guest",
+                           "nation",
+                           "climate", // 20
+                           "information",
+                           "cheese",
+                           "manual",
+                           "banana",
+                           "solution",
+                           "situation",
+                           "system",
+                           "machine",
+                           "cash",
+                           "bird", // 30
+                           "wolf",
+                           "dangerous",
+                           "towel",
+                           "gun",
+                           "life",
+                           "object",
+                           "competition",
+                           "hiking",
+                           "coordinate",
+                           "policy", // 40
+                           "balance",
+                           "action",
+                           "professor",
+                           "processor",
+                           "variable",
+                           "pointer",
+                           "function",
+                           "binary",
+                           "automation" // 49
+};
+
+#endif /* VOCABULARY_H_ */
